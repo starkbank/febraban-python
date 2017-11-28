c909ffcf1f087873be48fa3c53158c22ee7efb55
Deromir Neves Jr <deromir.neves@hummingbird.com.br>
2017-11-28 19:45:51 -0200

    Integrate with Pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='febraban',
+    packages=['febraban'],
+    version='0.1',
+    description='A library to generate files that conform to the FEBRABAN formats',
+    author='Hummingbird Product Studio',
+    author_email='deromir.neves@hummingbird.com.br',
+    url='https://github.com/HummingbirdStudio/febraban.git',
+    download_url='https://github.com/HummingbirdStudio/febraban/archive/v0.1.tar.gz',
+    keywords=['febraban', 'cnab', 'transfer', 'billing', 'bank']
+)
