9e56647c444a1a4ce342baccdd573172fb15f260
Rafael Castro <rafael.castro@hummingbird.com.br>
2017-12-12 14:19:05 -0200

    Update library version to 0.1.3

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 
+version = "0.1.3"
 setup(
     name='febraban',
     packages=['febraban'],
-    version='0.1.2',
+    version=version,
     description='A library to generate files that conform to the FEBRABAN formats',
     author='Hummingbird Product Studio',
     author_email='deromir.neves@hummingbird.com.br',
     url='https://github.com/HummingbirdStudio/febraban.git',
-    download_url='https://github.com/HummingbirdStudio/febraban/archive/v0.1.2.tar.gz',
+    download_url='https://github.com/HummingbirdStudio/febraban/archive/v%s.tar.gz' % version,
     keywords=['febraban', 'cnab', 'transfer', 'billing', 'bank']
 )
