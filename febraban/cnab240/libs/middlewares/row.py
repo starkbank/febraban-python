e50f55e1661e86afb94c4ab2e2adc01b2c459cd6
Rafael Stark <rafael@starkbank.com>
2018-07-16 18:44:20 -0300

    Fix middleware

--- febraban/cnab240/libs/middlewares/row.py
+++ febraban/cnab240/libs/middlewares/row.py
@@ -1,4 +1,4 @@
-import re
+from re import match
 from ...characterType import alphaNumeric, numeric
 
 
@@ -11,13 +11,13 @@ def validateFormatter(func):
            raise ValueError("charactersType must be alphaNumeric or numeric")
 
        if not isinstance(string, str):
-           raise ValueError("Type Value not allowed. Must be String. Used: %s " % str(type(string)))
+           raise ValueError("(%s,%s) is not an allowed value. It must be string" % (str(string), str(type(string))))
 
        regex = {
            alphaNumeric: r'^[A-Za-z0-9\s\-]+$',
            numeric:      r'^[0-9]+$'
        }[charactersType]
-       if string and not re.match(regex, string):
-           raise ValueError("You add a non %s value" % self.charactersType)
+       if string and not match(regex, string):
+           raise ValueError("You add %s that is a non %s value" % (string, charactersType))
        return func(self, string, charactersType, numberOfCharacters, defaultCharacter)
    return wrapper
