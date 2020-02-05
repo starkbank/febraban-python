fadb1d081910bf29c0a1c7d901d7bddb9caf495f
Vitor Gabriel <vitorsgobbi@hotmail.com>
2020-02-05 15:42:24 -0300

    added darf "setpayment" method

--- febraban/cnab240/itau/sispag/payment/darfPayment.py
+++ febraban/cnab240/itau/sispag/payment/darfPayment.py
@@ -1,15 +1,34 @@
-from febraban.cnab240.itau.sispag.payment.header import Header
-from febraban.cnab240.itau.sispag.payment.segmentNdarf import SegmentNdarf
-from febraban.cnab240.itau.sispag.payment.taxesTrailer import TaxesTrailer
+from .header import Header
+from .segmentNDarf import SegmentNDarf
+from .taxesTrailer import TaxesTrailer
 
 
 class DarfPayment:  # DARF NORMAL
 
     def __init__(self):
         self.header = Header(layoutNum="030")
-        self.segmentN = SegmentNdarf()
+        self.segmentN = SegmentNDarf()
         self.trailer = TaxesTrailer()
         self.trailer.setSumOthersAmountInCents(otherAmount="00000000000000")
+
+    def setPayment(self, **kwargs):
+        self.setSender(kwargs.get("sender"))
+        self.setTypeTaxId()
+        self.setSubscriptionId()
+        self.setPaymentId(kwargs.get("taxCode"))
+        self.setReferenceNumber(refNum="00000000000000001")  # ToDo check if is necessary
+        self.setTaxId(kwargs.get("taxId"))
+        self.setEvaluationDate(kwargs.get("referenceDate"))
+        self.setDueDate(kwargs.get("dueDate"))
+        self.setPaymentDate()
+        self.setAmount(kwargs.get("amount"))
+        self.setMainAmount(kwargs.get("amount"))
+        self.setInterestAmount(kwargs.get("interestAmount"))
+        self.setFineAmount(kwargs.get("fineAmount"))
+        self.setActualAmount(kwargs.get("amount"), kwargs.get("interestAmount"), kwargs.get("fineAmount"))
+        self.setTaxPayer(kwargs.get("sender"))
+        self.setIdentifier(kwargs.get("identifier"))
+        self.setInfo()
 
     def toString(self):
         return "\r\n".join((
@@ -47,16 +66,21 @@ class DarfPayment:  # DARF NORMAL
         self.segmentN.setMainAmount(mainAmount)
 
     def setFineAmount(self, fineAmount):
-        self.segmentN.setFineAmount(fineAmount)
-        self.trailer.setActualAmountInCents(actualAmount=fineAmount)
+        self.segmentN.setFineAmount(str(fineAmount))
 
     def setInterestAmount(self, interestAmount):
-        self.segmentN.setInterestAmount(interestAmount)
+        self.segmentN.setInterestAmount(str(interestAmount))
 
     def setAmount(self, amount):
         self.segmentN.setTotalAmount(amount)
         self.trailer.setSumAmountInCents(amount)
         self.trailer.setAmountInCents(amount)
+
+    def setActualAmount(self, amount, interestAmount, fineAmount):
+        actualAmount = amount
+        if interestAmount and fineAmount:
+            actualAmount = int(interestAmount) + int(fineAmount) + int(amount)
+        self.trailer.setActualAmountInCents(str(actualAmount))
 
     def setDueDate(self, dueDate):
         self.segmentN.setdueDate(dueDate)
@@ -64,8 +88,8 @@ class DarfPayment:  # DARF NORMAL
     def setPaymentDate(self):
         self.segmentN.setPaymentDate()
 
-    def setTaxPayer(self, payer):
-        self.segmentN.setTaxPayer(payer)
+    def setTaxPayer(self, sender):
+        self.segmentN.setTaxPayer(sender.name)
 
     def setPositionInLot(self, index):
         self.header.setPositionInLot(index)
