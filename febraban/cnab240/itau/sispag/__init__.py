4bdd4a6f59fb90c7026bdacc630bb5d8432632f9
Matheus Ferraz <matheus.ferraz@starkbank.com>
2020-05-27 20:08:51 -0300

    Added ISS and DAS payments in Sispag layout

--- febraban/cnab240/itau/sispag/__init__.py
+++ febraban/cnab240/itau/sispag/__init__.py
@@ -2,4 +2,6 @@ from .result.parser import PaymentParser
 from .payment.transfer import Transfer
 from .payment.chargePayment import ChargePayment
 from .payment.utilityPayment import UtilityPayment
+from .payment.dasPayment import DasPayment
+from .payment.issPayment import IssPayment
 from .file.file import File
